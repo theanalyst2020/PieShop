82f14e47f3ff0ba40731e53de43fde9bdc6e52ed
theanalyst2020 <theanalyst2020@hotmail.com>
2020-12-06 14:50:41 +0400

    Added an entire ViewModel to feed list View instead of viewbag and iEnumerator list + added a css file

--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PieShop.Models;
+using PieShop.ViewModels;
 
 namespace PieShop.Controllers
 {
@@ -19,8 +20,14 @@ namespace PieShop.Controllers
 
         public ViewResult ListPies()
         {
-                return View(PieInstance.AllPies);
-         }
+            //   ViewBag.availableCakes = "There are 50 options available";
+            //  return View(PieInstance.AllPies);
+            ListPiesViewModel listPiesViewModel = new ListPiesViewModel();
+            listPiesViewModel.AllPies = PieInstance.AllPies;
+            listPiesViewModel.CurrentCategory = "cupcakes";
+            listPiesViewModel.AvailableCakes = 6;
+            return View(listPiesViewModel);
+        }
 
 
     }
---
+++ PieShop/ViewModels/ListPiesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.ViewModels
+{
+    public class ListPiesViewModel
+    {
+        public IEnumerable<Pie> AllPies { get; set; }
+        public int  AvailableCakes { get; set; }
+        public String CurrentCategory { get; set; }
+    }
+}
